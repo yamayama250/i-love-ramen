ea43f2a52994de4c896bc084717aef66185b7c7e
yamayama <yamayama.dev@gmail.com>
2022-09-26 11:55:14 +0900

    雛形作成

---
+++ app/src/test/java/local/hal/st42/android/originalapp90014/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package local.hal.st42.android.originalapp90014
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
